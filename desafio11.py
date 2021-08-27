974ab414a4cf0c04ad54d6170db3b0b3722883a9
LeonardoMancilha <leonardomachado.sjc@gmail.com>
2021-08-27 09:22:05 -0300

    commit

---
+++ desafio11.py
@@ -0,0 +1,6 @@
+parede = float(input('Quanto de largura a sua parede tem?'))
+calculo = float(input('Quanto de altura ela tem?'))
+area = parede*calculo
+tinta = area/2
+print('A quantidade de tinta necessária para pintar é de:', tinta, 'litros', 'e a área da parede é de:', area,'Metros Quadrados')
+
